2c9240da232d08e921a86a913adc66b6a18f5881
pratishtha-bhatia <pratishtha.bhatia2805@gmail.com>
2020-01-10 15:09:53 +0530

    minor fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Pratu Ji Ka Blog`,
     navigation: [
       {
         title: `Blog`,
